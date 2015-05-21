bdce9541b4b441d5fa84771657dce8c56a6c07f8
Neo <1056835162@qq.com>
2015-05-22 02:01:15 +0800

    进一步完成exp，缺少结构体，数组，函数调用，缺少exp优化

--- Lab/Code/translate.c
+++ Lab/Code/translate.c
@@ -4,7 +4,7 @@
  * 翻译exp，其中的option表示place的主动、被动。 
  * 0：被动：place上层仅仅提供了位置，可以没有左值。你填写任何，上层会直接使用，比如填写立即数也可以。
  * 1：主动：place是由上层给定的，确定的变量，请将你的结果放入place
- * 如果place==NULL，并且option是被动“0”，则本层不要生成变量返回该exp的结果。
+ * 如果place==NULL,则本层不要生成变量返回该exp的结果。
  */
 void translate_exp(Node* h,char* place,int option);
 //翻译COND，由上层提供跳转label
@@ -122,9 +122,51 @@ void translate(Node* h)
 
 void translate_exp(Node* h,char* place,int option)
 {
+	if(h->child[0]->type==_LP)
+	{
+		return translate_exp(h->child[1],place,option);
+	}
+	else if(h->child_count==3 && h->child[1]->type==_ASSIGNOP)
+	{
+		char temp[32];
+		translate_exp(h->child[0],temp,0);
+		translate_exp(h->child[2],temp,1);
+		if(place!=NULL)
+		{
+			if(option==0)
+			{
+				strcpy(place,temp);
+			}
+			else
+			{
+				add_code(3,place,":=",temp);
+			}
+		}
+	}
+	else if(h-<child_count==3 && h->child[1]->type==_LP)
+	{
+		if(place==NULL)
+		{
+			char temp[32];
+			new_temp(temp);
+			if(strcmp(h->child[0]->name,"read")==0)
+				add_code(2,"READ",temp);
+			else
+				add_code(4,temp,":=","CALL",h->child[0]->name);
+		}
+		else
+		{
+			if(option==0)
+				new_temp(place);
+			if(strcmp(h->child[0]->name,"read")==0)
+				add_code(2,"READ",place);
+			else
+				add_code(4,place,":=","CALL",h->child[0]->name);
+		}
+	}
+	if(place==NULL)return;
 	if(h->child[0]->type==_INT)
 	{
-		if(place==NULL)return;
 		if(option==0)
 		{
 			sprintf(place,"#%d",h->child[0]->value_i);
@@ -136,23 +178,8 @@ void translate_exp(Node* h,char* place,int option)
 			add_code(3,place,":=",temp);
 		}
 	}
-	if(h->child[0]->type==_FLOAT)
+	else if(h->child[0]->type==_ID && h->child_count==1)
 	{
-		if(place==NULL)return;
-		if(option==0)
-		{
-			sprintf(place,"#%f",h->child[0]->value_f);
-		}
-		else
-		{
-			char temp[32];
-			sprintf(temp,"#%f",h->child[0]->value_f);
-			add_code(3,place,":=",temp);
-		}
-	}
-	if(h->child[0]->type==_ID && h->child_count==1)
-	{
-		if(place==NULL)return;
 		if(option==0)
 		{
 			strcpy(place,h->child[0]->name);
@@ -162,9 +189,147 @@ void translate_exp(Node* h,char* place,int option)
 			add_code(3,place,":=",h->child[0]->name);
 		}
 	}
-	if(h->child[0]->type==_LP)
+	else if(h->child_count==3 && (h->child[1]->type==_PLUS || h->child[1]->type==_MINUS || h->child[1]->type==_STAR || h->child[1]->type==_DIV))
 	{
-		return translate_exp(h->child[1],place,option);
+		char temp1[32],temp2[32];
+		translate_exp(h->child[0],temp1,0);
+		translate_exp(h->child[2],temp2,0);
+		if(((temp1[0]=='#' && temp1[1]=='0')||(temp2[0]=='#' && temp2[1]=='0')) && (h->child[1]->type==_STAR || h->child[1]->type==_DIV))
+		{
+			if(option==0)
+			{
+				place[0]='#';
+				place[1]='0';
+				place[2]='\0';
+			}
+			else
+			{
+				add_code(3,place,":=","#0");
+			}
+		}
+		else if(temp1[0]=='#' && temp2[0]=='#')
+		{
+			int i1=strtol(&temp1[1],NULL,10);
+			int i2=strtol(&temp2[1],NULL,10);
+			if(h->child[1]->type==_PLUS)
+			{
+				if(option==0)
+				{
+					sprintf(place,"#%d",i1+i2);
+				}
+				else
+				{
+					char temp[32];
+					sprintf(temp,"#%d",i1+i2);
+					add_code(3,place,":=",temp);
+				}
+			}
+			else if(h->child[1]->type==_MINUS)
+			{
+				if(option==0)
+				{
+					sprintf(place,"#%d",i1-i2);
+				}
+				else
+				{
+					char temp[32];
+					sprintf(temp,"#%d",i1-i2);
+					add_code(3,place,":=",temp);
+				}
+			}
+			else if(h->child[1]->type==_STAR)
+			{
+				if(option==0)
+				{
+					sprintf(place,"#%d",i1*i2);
+				}
+				else
+				{
+					char temp[32];
+					sprintf(temp,"#%d",i1*i2);
+					add_code(3,place,":=",temp);
+				}
+			}
+			else
+			{
+				if(option==0)
+				{
+					sprintf(place,"#%d",i1/i2);
+				}
+				else
+				{
+					char temp[32];
+					sprintf(temp,"#%d",i1/i2);
+					add_code(3,place,":=",temp);
+				}
+			}
+		}
+		else if(temp2[0]=='#' && temp2[1]=='0')
+		{
+			if(option==0)
+				strcpy(place,temp1);
+			else
+				add_code(3,place,":=",temp1);
+		}
+		else if(temp1[0]=='#' && temp1[1]=='0' && h->child[1]->type==_PLUS)
+		{
+			if(option==0)
+				strcpy(place,temp2);
+			else
+				add_code(3,place,":=",temp2);
+		}
+		else
+		{
+			char op[2];
+			op[1]='\0';
+			if(h->child[1]->type==_PLUS) op[0]='+';
+			else if(h->child[1]->type==_MINUS) op[0]='-';
+			else if(h->child[1]->type==_STAR) op[0]='*';
+			else op[0]='/';
+			if(option==0)
+			{
+				new_temp(place);
+			}
+			add_code(5,place,":=",temp1,op,temp2);
+		}
+	}
+	else if(h->child[0]->type==_MINUS)
+	{
+		char temp[32];
+		translate_exp(h->child[1],temp,0);
+		if(temp[0]=='#')
+		{
+			int i=strtol(&temp[1],NULL,10);
+			if(option==0)
+			{
+				sprintf(place,"#%d",-i);
+			}
+			else
+			{
+				char temp[32];
+				sprintf(temp,"#%d",-i);
+				add_code(3,place,":=",temp);
+			}
+		}
+		else
+		{
+			if(option==0)
+			{
+				new_temp(place);
+			}
+			add_code(5,place,":=","#0","-",temp);
+		}
+	}
+	else if(h->child[0]->type==_NOT || h->child[1]->type==_RELOP || h->child[1]->type==_AND || h->child[1]->type==_OR)
+	{
+		char label1[32],label2[32];
+		if(option==0)
+			new_temp(place);
+		add_code(3,place,":=","#0");
+		translate_cond(h,label1,label2);
+		add_code(3,"LABEL",label1,":");
+		add_code(3,place,":=","#1");
+		add_code(3,"LABEL",label2,":");
 	}
 }
 void translate_cond(Node*h,char* label_true,char* label_false)
